24fe63a1d5d0d623009c3307d72de1005f3914e4
Leonardo Davinc Silva <davinc131@hotmail.com>
2021-03-26 01:15:32 -0300

    Implementação do padrão de projeto singleton

---
+++ ImplementacaoSingleton/PadraoSingleton/CalcularValores.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementacaoSingleton.PadraoSingleton
+{
+  public class CalcularValores
+  {
+    private static CalcularValores _instance;
+
+    private CalcularValores() { }
+
+    public static CalcularValores Instance
+    {
+      get
+      {
+        if (_instance == null)
+          _instance = new CalcularValores();
+        return _instance;
+      }
+    }
+
+    public int Somar(int valor1, int valor2)
+    {
+      return valor1 + valor2;
+    }
+
+    public int Subtrair(int valor1, int valor2)
+    {
+      return valor1 - valor2;
+    }
+
+    public int Multiplicar(int valor1, int valor2)
+    {
+      return valor1 * valor2;
+    }
+
+    public int Dividir(int valor1, int valor2)
+    {
+      return valor1 / valor2;
+    }
+  }
+}
---
+++ ImplementacaoSingleton/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ImplementacaoSingleton.PadraoSingleton;
+
+namespace ImplementacaoSingleton
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      while (true)
+      {
+        Console.Write("Informe o primeiro valor: ");
+        string valor1 = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Informe o segundo valor: ");
+        string valor2 = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Selecione o tipo de operação - Soma(1), Subtração(2), Multiplicação(3), Divisão(4): ");
+        string tipoOperacao = Console.ReadLine();
+
+        switch (tipoOperacao)
+        {
+          case "1":
+            Console.WriteLine("Resultado: " + CalcularValores.Instance.Somar(int.Parse(valor1), int.Parse(valor2)));
+            break;
+          case "2":
+            Console.WriteLine("Resultado: " + CalcularValores.Instance.Subtrair(int.Parse(valor1), int.Parse(valor2)));
+            break;
+          case "3":
+            Console.WriteLine("Resultado: " + CalcularValores.Instance.Multiplicar(int.Parse(valor1), int.Parse(valor2)));
+            break;
+          case "4":
+            Console.WriteLine("Resultado: " + CalcularValores.Instance.Dividir(int.Parse(valor1), int.Parse(valor2)));
+            break;
+          default:
+            Console.WriteLine("Operação não reconhecida.");
+            break;
+        }
+        Console.ReadKey();
+      }
+    }
+  }
+}
